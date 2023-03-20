ecfa602e6180558d05eda6c73fb359d5db64d077
Djazia djeridi <djaziadjeridi@Air-de-Djazia.lan>
2023-03-20 11:29:36 +0100

    recuper les articles et je les afficher

--- front/js/script.js
+++ front/js/script.js
@@ -1,1 +1,40 @@
-console.log('hello')
+
+
+/**
+ * 1 recuperer les articles via l'API
+ * 2 construire un html
+ * 3 injecter html dans le dom=generer l'html
+ * */
+
+fetch("http://localhost:3000/api/products")
+.then(function(reponse){
+    if(reponse.ok){
+        return reponse.json()
+    }
+})
+.then(function(data){
+ displayProducts(data)
+})
+.catch(function(err){
+    console.log(err)
+})
+
+function displayProducts(products){
+    for (let i = 0; i < products.length; i++) {
+            const product = products[i];
+            document.querySelector('#items').innerHTML +=`
+    
+            <a href="./product.html?id=${product._id}">
+            <article>
+              <img src="${product.imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">}${product.description}</p>
+            </article>
+            </a>
+             `   
+    }
+}
+    
+
+ 
+
