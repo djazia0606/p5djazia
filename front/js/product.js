3eed112b6b21f8ab1223119cf56acf7366752290
Djazia djeridi <djaziadjeridi@Air-de-Djazia.lan>
2023-04-12 13:13:28 +0200

    localstorage

--- front/js/product.js
+++ front/js/product.js
@@ -53,13 +53,11 @@ function displayProduct(data){
     
     let addOption = document.querySelector('#colors')
      
-    for (let i = 0; i=data.color.length; i++){
+    for (let i = 0; i<data.colors.length; i++){
         
-        
-    
         const option = document.createElement('option'); 
-        option.value = [data.color]; 
-        option.textContent = color; 
+        option.value = data.colors[i]; 
+        option.textContent = data.colors[i]; 
         addOption.appendChild(option); 
         
     }
@@ -68,4 +66,30 @@ function displayProduct(data){
     
 
 }
+    const addToCartButton = document.getElementById("addToCart");
+
+    addToCartButton.addEventListener("click", function() {
+    const userInput = prompt("Veuillez saisir une valeur :");
+    console.log(userInput);
+    });
+
+    const addToCartButton = document.getElementById("addToCart");
+
+    addToCartButton.addEventListener("click", function() {
+    const id = "123"; // à remplacer par l'id de l'article
+    const color = "rouge"; // à remplacer par la couleur sélectionnée par l'utilisateur
+    const quantity = 1; // à remplacer par la quantité sélectionnée par l'utilisateur
+    const cartItem = [id, color, quantity];
+  
+  // Récupération des éléments du panier dans le localStorage (s'il existe)
+  let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+  
+  // Ajout du nouvel élément dans le tableau d'éléments du panier
+  cartItems.push(cartItem);
+  
+  // Enregistrement du tableau d'éléments du panier dans le localStorage
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+});
+
+
     
